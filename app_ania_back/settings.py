1e36f5dea4f8219b7a424e32bd1b005371ca9b2b
Sebastien0506 <124361814+Sebastien0506@users.noreply.github.com>
2026-02-17 22:54:52 +0100

    Changement de la base de données

--- app_ania_back/settings.py
+++ app_ania_back/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'app_ania_back.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'app_bot',
+        'OPTION': {
+            "service" : "my_service"
+        }
     }
 }
 
